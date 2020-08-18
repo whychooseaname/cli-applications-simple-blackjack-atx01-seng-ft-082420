0e4956bcaeba60c23163afea57e125b0a7f4f56c
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-18 01:24:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -67,7 +67,7 @@ def runner
   # code runner here
   welcome 
   card_total = initial_round
-  while card_total < 22
+  while card_total < 21
     hit? (card_total)
     display_card_total (card_total)
   end
