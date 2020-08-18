e418fb0d6bb2e7840158f67183b6d0e329b31001
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-18 01:17:49 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -71,7 +71,7 @@ def runner
     hit? (card_total)
     display_card_total (card_total)
   else
-    display_card_total (card_total)
-    end_game
+    #display_card_total (card_total)
+    end_game (card_total)
   end
 end
